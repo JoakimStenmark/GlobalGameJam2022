9cb43cbb5b69e2241bf04ebd3fda38a0928822cf
Joakim Stenmark <Jocke.Stenmark@live.se>
2022-01-29 11:47:58 +0100

    Player controller tests

---
+++ Assets/Joakim/TestCameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform followTarget;
+    [SerializeField] float heightOffset = 10;
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Camera.main.transform.position = new Vector3(transform.position.x, followTarget.position.y + heightOffset, transform.position.z);
+        Camera.main.transform.LookAt(followTarget);
+
+
+    }
+}
