79d1a6e3e595a5477000a747171de7fabb462b11
Adam Forsell <adamforsell2@hotmail.com>
2022-01-29 11:55:12 +0100

    Update HealthController.cs

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -32,12 +33,28 @@ public class HealthController : MonoBehaviour
 	{
 		CurrentHealth = startHealth;
 
+		OrganizeHealthPointSlotsUI();
+
+		SetAmountOfActiveHealthPoints(startHealth);
+
+		if (startHealth == healthPointsSlots.Length)
+			return;
+
+		for (int i = startHealth; i < healthPointsSlots.Length; i++)
+		{
+			SetActiveHealthPoint(i, false);
+		}
+	}
+
+	private void OrganizeHealthPointSlotsUI()
+	{
 		var newPosition = Vector3.zero;
+
 		for (int i = 0; i < healthPointsSlots.Length; i++)
 		{
 			healthPointsSlots[i] = UIHealthContainer.transform.GetChild(i).gameObject;
 			var rectTransform = healthPointsSlots[i].GetComponent<RectTransform>();
-			
+
 			if (i == 0)
 			{
 				newPosition = rectTransform.position;
@@ -56,21 +73,14 @@ public class HealthController : MonoBehaviour
 
 			rectTransform.position = newPosition;
 		}
-		for (int i = 0; i < startHealth; i++)
-		{
-			SetActiveHealthPoints(i, true);
-		}
-
-		if (startHealth == healthPointsSlots.Length)
-			return;
-
-		for (int i = startHealth; i < healthPointsSlots.Length; i++)
-		{
-			SetActiveHealthPoints(i, false);
-		}
 	}
 
-	public void SetActiveHealthPoints(int index, bool active)
+	/// <summary>
+	/// Set the activation of a HealthPoint specified with index.
+	/// </summary>
+	/// <param name="index"></param>
+	/// <param name="active"></param>
+	public void SetActiveHealthPoint(int index, bool active)
 	{
 		healthPointsSlots[index].SetActive(active);
 	}
@@ -88,5 +98,31 @@ public class HealthController : MonoBehaviour
 				return;
 			}
 		}
+	}
+
+	public void SetAmountOfActiveHealthPoints(int amount)
+	{
+		for (int i = 0; i < amount; i++)
+		{
+			healthPointsSlots[i].SetActive(true);
+		}
+	}
+
+	/// <summary>
+	/// Use this to Update the CurrentHealth of the player, including the UI elements/visuals.
+	/// </summary>
+	public void UpdateCurrentHealth()
+	{
+		CurrentHealth = 0;
+
+		for (int i = 0; i < healthPointsSlots.Length; i++)
+		{
+			if (healthPointsSlots[i].activeSelf)
+				CurrentHealth++;
+			else
+				break;
+		}
+
+		SetAmountOfActiveHealthPoints(CurrentHealth);
 	}
 }
