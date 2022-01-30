1fbebaeca4b1fe1126422439f590fa013a93c663
Adam Forsell <adamforsell2@hotmail.com>
2022-01-30 15:00:59 +0100

    there is now a highscore saved and shown both in start scene and game over state in gamescene

--- Assets/Scripts/AdamsTemp/SceneController.cs
+++ Assets/Scripts/AdamsTemp/SceneController.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+	[SerializeField] private TextMeshProUGUI highscoreTxt;
+	private int highscore;
+
+	private void Awake()
+	{
+		PlayerPrefs.GetInt("Highscore", highscore);
+		highscoreTxt.text = $"Highscore: {highscore}";
+	}
+
 	public void LoadScene(int scene)
 	{
 		SceneManager.LoadScene(scene);
