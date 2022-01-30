8bc46820019e13ed50b4c5e836856eeadc696b4c
Adam Forsell <adamforsell2@hotmail.com>
2022-01-30 14:28:18 +0100

    Built a draft of a Start Menu

---
+++ Assets/Scripts/AdamsTemp/SceneController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+	public void LoadScene(int scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+
+	public void LoadScene(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
